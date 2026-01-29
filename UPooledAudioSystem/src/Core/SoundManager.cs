663f8189f504a19ea122ee8ebc12394c875e55dc
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-29 11:12:42 -0400

    Added soft-static sources and a sound list.

--- UPooledAudioSystem/src/Core/SoundManager.cs
+++ UPooledAudioSystem/src/Core/SoundManager.cs
@@ -1,5 +1,6 @@
 // Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
 
+using ca.ScottishDwarfStudio.UPooledAudioSystem.Util;
 using UnityEngine;
 
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
@@ -8,9 +9,17 @@ public sealed class SoundManager:MonoBehaviour
 {
     internal PoolSystem PoolSystem => poolSystem;
     PoolSystem poolSystem;
+    
+    AudioSource musicSource;
+    AudioSource staticFxSource;
+    
+    [SerializeField] SoundList sounds;
 
     void Start()
     {
-        throw new NotImplementedException();
+        musicSource = (new GameObject().AddComponent(typeof(AudioSource)) as AudioSource)!;
+        staticFxSource = (new GameObject().AddComponent(typeof(AudioSource)) as AudioSource)!;
+        musicSource.transform.SetParent(transform);
+        staticFxSource.transform.SetParent(transform);
     }
 }
