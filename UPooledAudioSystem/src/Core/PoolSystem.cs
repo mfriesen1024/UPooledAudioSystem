5da2c7c8d6185b4efdc956da9b604a1855a8a6aa
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-29 10:27:18 -0400

    Added copyright notice.

--- UPooledAudioSystem/src/Core/PoolSystem.cs
+++ UPooledAudioSystem/src/Core/PoolSystem.cs
@@ -1,3 +1,5 @@
+// Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (<mfriesen1024@gmail.com>)
+
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
 
 internal class PoolSystem
--- UPooledAudioSystem/src/Core/SoundManager.cs
+++ UPooledAudioSystem/src/Core/SoundManager.cs
@@ -1,3 +1,5 @@
+// Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
+
 using UnityEngine;
 
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
