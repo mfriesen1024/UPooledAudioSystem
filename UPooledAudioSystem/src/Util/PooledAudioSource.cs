20f773e820c6256d50d8c886a30a4da528ff0ab6
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-30 11:55:00 -0400

    Created PooledAudioSource.cs

---
+++ UPooledAudioSystem/src/Util/PooledAudioSource.cs
@@ -0,0 +1,44 @@
+// Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
+
+using ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
+using UnityEngine;
+
+namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Util;
+
+[RequireComponent(typeof(AudioSource))]
+internal class PooledAudioSource:MonoBehaviour
+{
+    SoundManager soundManager => SoundManager.Instance;
+    
+    bool initialized;
+    
+    internal AudioSource audioSource;
+    internal AudioClip clip;
+    internal bool shouldDestroyOnFinish;
+
+    int ticksLeft;
+
+    internal void Init()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        if (shouldDestroyOnFinish)
+        {
+            ticksLeft = (int)clip.length * 50;
+        }
+        
+        initialized = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (!initialized) return;
+        ticksLeft--;
+
+        if (ticksLeft > 0) return;
+        if (shouldDestroyOnFinish) Destroy(gameObject);
+        else gameObject.SetActive(false);
+    }
+}
