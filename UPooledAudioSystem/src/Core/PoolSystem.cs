af928e8b99021cd0a65920298fa2a1db1448b71f
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-30 11:29:05 -0400

    Added empty init function to pool system.

--- UPooledAudioSystem/src/Core/PoolSystem.cs
+++ UPooledAudioSystem/src/Core/PoolSystem.cs
@@ -1,8 +1,13 @@
 // Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (<mfriesen1024@gmail.com>)
 
+using UnityEngine;
+
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
 
-internal class PoolSystem
+internal class PoolSystem:MonoBehaviour
 {
-    
+    internal void Init(int poolSize, bool deleteUnusedOverflow)
+    {
+        throw new NotImplementedException();
+    }
 }
