fc43542d5bf094403f3e2a3778e5d4d423c111de
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-30 12:17:08 -0400

    Created empty debug class for unit testing.

---
+++ UPooledAudioSystem/src/Debug/PoolDebugger.cs
@@ -0,0 +1,14 @@
+// Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
+
+using UnityEngine;
+
+namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Debug;
+
+// This is effectively a unit test for the pooling system.
+internal class PoolDebugger:MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
