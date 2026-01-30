7465dd2f259bf268d744ca195b71cee0d873ae24
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-01-30 11:27:31 -0400

    added fields for pool system and setup pool system init.

--- UPooledAudioSystem/src/Core/SoundManager.cs
+++ UPooledAudioSystem/src/Core/SoundManager.cs
@@ -2,6 +2,7 @@
 
 using ca.ScottishDwarfStudio.UPooledAudioSystem.Util;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
 
@@ -12,14 +13,23 @@ public sealed class SoundManager:MonoBehaviour
     
     AudioSource musicSource;
     AudioSource staticFxSource;
-    
+
+    [Header("Performance")] object crlf;
+    [SerializeField] int poolSize = 10;
+    [SerializeField] bool deleteUnusedOverflow=true; 
+
+    [Header("Sound Data")] object crlf2;
     [SerializeField] SoundList sounds;
 
     void Start()
     {
-        musicSource = (new GameObject().AddComponent(typeof(AudioSource)) as AudioSource)!;
-        staticFxSource = (new GameObject().AddComponent(typeof(AudioSource)) as AudioSource)!;
+        musicSource = (new GameObject("MusicSource").AddComponent(typeof(AudioSource)) as AudioSource)!;
+        staticFxSource = (new GameObject("StaticFXSource").AddComponent(typeof(AudioSource)) as AudioSource)!;
         musicSource.transform.SetParent(transform);
         staticFxSource.transform.SetParent(transform);
+
+        poolSystem = (new GameObject("PooledSources").AddComponent(typeof(PoolSystem)) as PoolSystem)!;
+        poolSystem.transform.SetParent(transform);
+        poolSystem.Init(poolSize, deleteUnusedOverflow);
     }
 }
