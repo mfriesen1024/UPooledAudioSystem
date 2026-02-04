7ace9582b873135e2438230f1cb5dd744e1c966f
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-02-04 10:58:00 -0400

    Wrote unit tests for pooling system.

--- UPooledAudioSystem/src/Debug/PoolDebugger.cs
+++ UPooledAudioSystem/src/Debug/PoolDebugger.cs
@@ -1,14 +1,56 @@
 // Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
 
+using ca.ScottishDwarfStudio.UPooledAudioSystem.Core;
+using ca.ScottishDwarfStudio.UPooledAudioSystem.Util;
 using UnityEngine;
 
 namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Debug;
 
 // This is effectively a unit test for the pooling system.
+[RequireComponent(typeof(SoundManager))]
 internal class PoolDebugger:MonoBehaviour
 {
+    [SerializeField] SoundList bla;
+    [SerializeField] SoundManager soundManager;
+    
     void Start()
     {
-        
+        soundManager ??= GetComponent<SoundManager>();
+        soundManager.Sounds = bla;
+
+        try
+        {
+            PlayerTest();
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError("Base player unit test failed. See exception below.");
+            UnityEngine.Debug.LogException(e);
+        }
+
+        try
+        {
+            PoolTest();
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError("Pool unit test failed. See exception below.");
+            UnityEngine.Debug.LogException(e);
+        }
+    }
+
+    void PlayerTest()
+    {
+        soundManager.PlaySound(bla.menuMusic);
+        soundManager.PlaySound(bla.chestClose);
+        soundManager.PlaySound(bla.playerHit);
+    }
+
+    void PoolTest()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            soundManager.PlaySound(bla.playerHit);
+        }
     }
 }
