478d44bfa49696e3c73ae9fe6af1e63614e217dc
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-02-12 14:33:56 -0400

    Prevent NREs while loading.

--- UPooledAudioSystem/src/Ext/EventHelper.cs
+++ UPooledAudioSystem/src/Ext/EventHelper.cs
@@ -49,7 +49,8 @@ internal class EventHelper
 #if CAN_LINK
     void LoadSounds()
     {
-        sounds = Instance.Sounds;
+        sounds = Instance?.Sounds;
+        if(sounds == null) Debug.LogWarning("Sounds not found, but this might have happened OnValidate. If this happens at runtime its bad.");
     }
 
     void ButtonPressedNormal()
