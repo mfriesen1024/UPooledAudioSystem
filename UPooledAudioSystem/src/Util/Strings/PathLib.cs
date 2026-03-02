b4805c7d1ceb38f41e267a53a6cb1f53c2b04439
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2026-03-02 09:16:50 -0400

    Account for alternate project directory.

--- UPooledAudioSystem/src/Util/Strings/PathLib.cs
+++ UPooledAudioSystem/src/Util/Strings/PathLib.cs
@@ -1,12 +1,12 @@
-// Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
-
-namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Util.Strings;
-
-internal static class PathLib
-{
-    public const string WebManifestUrl = "https://raw.githubusercontent.com/mfriesen1024/DwarfMusic/refs/heads/stable/manifest";
-    public const string LocalLoadPath = "../DwarfMusic/Output/";
-    public const string LocalManifestPath = "../DwarfMusic/manifest";
-    public const string MusDropPath = "./Assets/Sound/Music/";
-    public const string SfxDropPath = "./Assets/Sound/SFX/";
-}
+// // Copyright © 2026 ScottishDwarfStudios under licence from mFriesen1024 (mfriesen1024@gmail.com)
+//
+// namespace ca.ScottishDwarfStudio.UPooledAudioSystem.Util.Strings;
+//
+// internal static class PathLib
+// {
+//     public const string WebManifestUrl = "https://raw.githubusercontent.com/mfriesen1024/DwarfMusic/refs/heads/stable/manifest";
+//     public const string LocalLoadPath = "../DwarfMusic/Output/";
+//     public const string LocalManifestPath = "../DwarfMusic/manifest";
+//     public const string MusDropPath = "./Assets/Sound/Music/";
+//     public const string SfxDropPath = "./Assets/Sound/SFX/";
+// }
